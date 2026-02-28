8d1c6b43cb4f216bdcd2a292c735826ab492e771
David King <davidking@Davids-MacBook-Pro.local>
2026-02-28 22:01:25 +0000

    home page and extensions

---
+++ app/extensions.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import scoped_session, sessionmaker
+import lyricsgenius
+
+engine = None
+db_session = None
+api_conn = None
+
+def init_db(config):
+    global engine, db_session
+    db_uri = f'postgresql://{config.db_user}:{config.db_pass}@{config.db_host}:{config.db_port}/{config.db_name}'
+    engine = create_engine(db_uri)
+    try:
+        session = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+        db_session = scoped_session(session)
+
+        query = text('SELECT version();')
+        result = db_session.execute(query)
+
+        db_version = result.fetchone()[0]
+        print(f"Connected to PostgreSQL! Server version: {db_version}")
+        return db_session, engine
+
+    except Exception as e:
+        print(f"Error connecting to PostgreSQL database: {e}")
+
+
+def init_api_client(config):
+    global api_conn
+    try:
+        lg_conn = lyricsgenius.Genius(config.access_token, remove_section_headers=True, timeout=None)
+        api_conn = lg_conn
+
+    except Exception as e:
+        print(f"Error connecting to lyrics genius api: {e}")
--- app/routes.py
+++ app/routes.py
@@ -1,63 +1,65 @@
-from flask import render_template, request, redirect, url_for, Blueprint, jsonif
-from werkzeug.utils import secure_filename
-import os
+from flask import render_template, Blueprint
+
+# from flask import request, redirect, url_for, Blueprint, jsonify, current_app)
+# from werkzeug.utils import secure_filename
+# import os
 
 bp = Blueprint("main", __name__)
 
 # default root
 @bp.route('/')
 def index():
-    return render_template('template.html', title='HomePage', name='David King', messages=['Hello', 'Hi', 'Hey'])
+    return render_template('template.html')
 
-# form examples
-@bp.route('/contact')
-def contact():
-    return render_template('contact.html')
+# # form examples
+# @bp.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+#
+# @bp.route('/handle_form', methods=['POST'])
+# def handle_form():
+#     name = request.form['name']
+#     email = request.form['email']
+#     return redirect(url_for('thank_you'))
+#
+# # form file upload
+# @bp.route('/handle_file_upload', methods=['POST'])
+# def handle_file_upload():
+#     file = request.files['file']
+#     if file and allowed_file(file.filename):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(bp.config['UPLOAD_FOLDER'], filename))
+#         return redirect(url_for('uploaded_file', filename=filename))
+#     return 'File upload unsuccessful'
+#
+# ALLOWED_EXTENSIONS = {"csv", "tsv", "txt", "json", "xlsx", "xls", "png", "jpg", "jpeg", "pdf"}
+# def allowed_file(filename: str) -> bool:
+#     """Return True if filename has an allowed extension."""
+#     return bool(filename and "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS)
+#
+# # dynamic routes: show user profile, post id
+# @bp.route('/user/<username>')
+# def show_profile(username):
+#     return f'username: {username}'
+#
+# @bp.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     return f'post: {post_id}'
+#
+# # error handling
+# @bp.errorhandler(404)
+# def page_not_found(error):
+#     return "This page does not exist.", 404
+#
+# @bp.errorhandler(500)
+# def internal_server_error(error):
+#     return "Internal server error.", 500
 
-@bp.route('/handle_form', methods=['POST'])
-def handle_form():
-    name = request.form['name']
-    email = request.form['email']
-    return redirect(url_for('thank_you'))
-
-# form file upload
-@bp.route('/handle_file_upload', methods=['POST'])
-def handle_file_upload():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(bp.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file', filename=filename))
-    return 'File upload unsuccessful'
-
-ALLOWED_EXTENSIONS = {"csv", "tsv", "txt", "json", "xlsx", "xls", "png", "jpg", "jpeg", "pdf"}
-def allowed_file(filename: str) -> bool:
-    """Return True if filename has an allowed extension."""
-    return bool(filename and "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS)
-
-# dynamic routes: show user profile, post id
-@bp.route('/user/<username>')
-def show_profile(username):
-    return f'username: {username}'
-
-@bp.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f'post: {post_id}'
-
-# error handling
-@bp.errorhadnler(404)
-def page_not_found(error):
-    return "This page does not exist.", 404
-
-@bp.errorhadnler(500)
-def internal_server_error(error):
-    return "Internal server error.", 500
-
-# generates urls
-with bp.test_request_context():
-    print(url_for('index'))
-    print(url_for(show_profile))
-    print(url_for(show_profile))
+# # generates urls
+# with current_app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for(show_profile))
+#     print(url_for(show_profile))
 
 # http methods
 # @app.route('/login', method=['GET', 'POST'])
