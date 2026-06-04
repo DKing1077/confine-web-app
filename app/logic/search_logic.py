4bd2f76d0222539b987ffe9a2e28a2bd88c4e93e
David King <davidking@Davids-MacBook-Pro.local>
2026-06-04 02:05:21 +0100

    syntax clean

--- app/logic/search_logic.py
+++ app/logic/search_logic.py
@@ -1,25 +1,50 @@
 from app.cache import search_cache_set, search_cache_get, search_cache_key
 from app.services import MusixMatch
 from flask import current_app
-from db.search_db import *
+from app.db import *
 import json
 
 
+def search_cache(db_session, user_id, artist_input, track_input, search_text):
+    cache_key = search_cache_key(artist_input, track_input)
+    cached_data = search_cache_get(cache_key)
+
+    if cached_data:
+        current_app.logger.info(
+            "Cache hit\n"
+            "Key: %s\n"
+            "Type: %s\n"
+            "Data:\n%s",
+            cache_key,
+            type(cached_data).__name__,
+            json.dumps(cached_data, default=str, indent=2)
+        )
+
+        add_search_result(db_session, user_id, search_text, cached_data)
+        logger_message(cached_data, 'search cache', artist_input, track_input)
+        return cached_data, None
+
+    else:
+        current_app.logger.info("cache miss for key: %s", cache_key)
+        return None, cache_key
+
+
 def search_pipeline(db_session, user_id, artist_input, track_input, search_text, cache_key):
-    search_result = process_search(db_session, artist_input, track_input)
+    search_result = search_fetch(db_session, artist_input, track_input)
+    search_cache_set(cache_key, search_result)
+
     current_app.logger.info(
         "Search result (%s): %s",
         type(search_result).__name__,
         json.dumps(search_result, default=str)
     )
-    add_search_result(db_session, user_id, search_text, search_result)
-    search_cache_set(cache_key, search_result)
 
+    add_search_result(db_session, user_id, search_text, search_result)
     current_app.logger.info("cache set for key: %s", cache_key)
     return search_result
 
 
-def process_search(db_session, artist_input, track_input):
+def search_fetch(db_session, artist_input, track_input):
     db_classes, api_flag = db_lookup(db_session, artist_input, track_input)
     if api_flag:
         api_client = MusixMatch(api_key=current_app["MUSIXMATCH_APIKEY"])
@@ -32,33 +57,14 @@ def process_search(db_session, artist_input, track_input):
         db_insert(db_session, classes_parsed)
         api_classes = serialize_tracks(classes_parsed, 'api')
 
-        logger_message(api_classes, True, artist_input, track_input)
+        logger_message(api_classes, 'api', artist_input, track_input)
         return api_classes
 
-    logger_message(db_classes, False, artist_input, track_input)
+    logger_message(db_classes, 'postgres', artist_input, track_input)
     return db_classes
 
 
-def search_cache(artist_input, track_input):
-    cache_key = search_cache_key(artist_input, track_input)
-    cached_data = search_cache_get(cache_key)
-    if cached_data:
-        current_app.logger.info(
-            "Cache hit\n"
-            "Key: %s\n"
-            "Type: %s\n"
-            "Data:\n%s",
-            cache_key,
-            type(cached_data).__name__,
-            json.dumps(cached_data, default=str, indent=2)
-        )
-        return cached_data, cache_key
-    else:
-        current_app.logger.info("cache miss for key: %s", cache_key)
-        return None, cache_key
-
-
-def logger_message(return_var, flag, artist_input=None, track_input=None):
+def logger_message(return_var, method, artist_input=None, track_input=None):
     if artist_input and track_input:
         search_type = 'track search'
         search_values = f'{artist_input} - {track_input}'
@@ -66,10 +72,6 @@ def logger_message(return_var, flag, artist_input=None, track_input=None):
         search_type = 'artist search'
         search_values = f'{artist_input}'
 
-    if flag:
-        method = 'musixmatch api'
-    else:
-        method = 'postgres lookup'
     sample = return_var[0]
 
     current_app.logger.info('the work flow used : %s', method)
