c3297d2e50edf64a85be2af44dec44ea516f884f
David King <davidking@Davids-MacBook-Pro.local>
2026-06-28 22:18:42 +0100

    process button setup

--- app/services/openrouter.py
+++ app/services/openrouter.py
@@ -1,7 +1,8 @@
+from app.prompts import classify_search_messages, classify_concepts_message
 from openai import OpenAI
+import logging
 import json
 import re
-import logging
 
 logger = logging.getLogger(__name__)
 
@@ -17,35 +18,10 @@ class AIService:
         )
 
     def parse_search(self, search):
+        messages = classify_search_messages(search)
         response = self.client.chat.completions.create(
             model=self.model,
-            messages=[
-                {
-                    "role": "system",
-                    "content": (
-                        "Classify the search input into the correct artist and track names:\n\n"
-                        "Schema:\n"
-                        "{\n"
-                        '  "artist": string\n'
-                        '  "track": string, None\n'
-                        "}\n\n"
-                        "Rules:\n"
-                        "- Return the artist and track name exactly the same as its stored on spotify, search and check its the same\n"
-                        "- Return JSON ONLY\n"
-                        "- Do NOT include markdown.\n"
-                        "- Do NOT wrap output in ``` or ```json.\n\n"
-                        "Make sure you carefully check and search for an artists name in the input\n"
-                        "E.G logic - everyday should not be returned as track : logic everyday\n"
-                        "Make sure to place apostrophes exactly the same as its stored on spotify, search and check its the same\n"
-                        "E.G drake - gods plan should return the track name : God's Plan\n"
-                        "Make sure to capitalize the letters exactly the same as its stored on spotify, search and check its the same\n"
-                        "E.G \"NOKIA\" not \"nokia\" \"thank u, next\" not \"Thank you next\"\n"
-                        "Make sure you carefully check and search for the name of a track in the input\n"
-                        "E.G \"drake\" is just the name of an artist and the track should be returned as None"
-                    )
-                },
-                {"role": "user", "content": search},
-            ],
+            messages=messages,
             temperature=0
         )
         res = response.choices[0].message.content
@@ -64,6 +40,16 @@ class AIService:
             track_input = None
 
         return artist_input, track_input
+
+
+    def get_concepts(self, track_lyrics):
+        messages = classify_concepts_message(track_lyrics)
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            temperature=0
+        )
+        logger.info('track concepts response: ', response)
 
 
 
